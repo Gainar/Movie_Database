744d9ee3cade6560addf5f486aacd24c1c4faa54
Gainar Dan <dgainar@isoftwaresystems.com>
2018-08-14 14:41:28 +0300

    WebAPi

---
+++ DAL/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Movie
+    {
+        public Guid Pk_Movie_Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Type { get; set; }
+        public int Rating { get; set; }
+        public Guid Fk_Creator_Id { get; set; }
+    }
+}
